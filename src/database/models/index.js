7eaec3954f933ad7b0c6c050c9b2ef086ebf3307
Isaac <isaacmagno98@gmail.com>
2024-04-05 17:11:16 -0300

    add relationship

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,43 +1,82 @@
-'use strict';
+const Authentication = require("./authentication");
+const User = require("./user");
+const Address = require("./address");
+const Order = require("./order");
+const OrderItem = require("./orderItem");
+const Product = require("./product");
+const Category = require("./category");
+const Tag = require("./tag");
+const DiscountCupom = require("./discountCupom");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+// Relações entre as tabelas, conforme especificado anteriormente
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+// Relação 1:1 entre Authentication e User
+Authentication.hasOne(User, {
+  foreignKey: "authenticationId",
+  as: "user",
+});
+User.belongsTo(Authentication, {
+  foreignKey: "authenticationId",
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// Relação 1:n entre User e Address
+User.hasMany(Address, {
+  foreignKey: "userId",
+  as: "addresses",
+});
+Address.belongsTo(User, {
+  foreignKey: "userId",
+});
 
-module.exports = db;
+// Relação 1:n entre User e Order
+User.hasMany(Order, {
+  foreignKey: "userId",
+  as: "orders",
+});
+Order.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+// Relação 1:n entre Order e OrderItem
+Order.hasMany(OrderItem, {
+  foreignKey: "orderId",
+  as: "orderItems",
+});
+OrderItem.belongsTo(Order, {
+  foreignKey: "orderId",
+});
+
+// Relação 1:n entre Category e Product
+Category.hasMany(Product, {
+  foreignKey: "categoryId",
+  as: "products",
+});
+Product.belongsTo(Category, {
+  foreignKey: "categoryId",
+});
+
+// Relação N:N entre Tag e Product através da tabela ProductTag
+Tag.belongsToMany(Product, {
+  through: "ProductTag",
+  foreignKey: "tagId",
+  otherKey: "productId",
+  as: "products",
+});
+Product.belongsToMany(Tag, {
+  through: "ProductTag",
+  foreignKey: "productId",
+  otherKey: "tagId",
+  as: "tags",
+});
+
+module.exports = {
+  Authentication,
+  User,
+  Address,
+  Order,
+  OrderItem,
+  Product,
+  Category,
+  Tag,
+  DiscountCupom,
+};
